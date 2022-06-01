5e42d36663694ca8da19edd80079a5164a603253
spbgzh <zjjhgzh@gmail.com>
2022-06-01 10:50:01 +0300

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Helwd\n");
         System.out.print("Helwd\n");
-
+    //21312312
+       // AA
     }
 }
